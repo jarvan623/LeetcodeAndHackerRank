99493ce84caef37483592b8ed0f56ea58541f772
jarvan623 <372993105@qq.com>
2022-09-12 22:58:29 +1000

    BST to greater tree

---
+++ LeetCode/src/Q538.java
@@ -0,0 +1,31 @@
+public class Q538 {
+
+  /*Given the root of a Binary Search Tree (BST), convert it to a Greater Tree
+  such that every key of the original BST is changed to the original key plus
+  the sum of all keys greater than the original key in BST.
+  As a reminder, a binary search tree is a tree that satisfies these constraints:
+  The left subtree of a node contains only nodes with keys less than the node's key.
+  The right subtree of a node contains only nodes with keys greater than the node's key.
+  Both the left and right subtrees must also be binary search trees.
+  https://leetcode.cn/problems/convert-bst-to-greater-tree*/
+  public static void main(String[] args) {
+    int[] vals = new int[] {4, 1, 6, 0, 2, 5, 7, -1, -1, -1, 3, -1, -1, -1, 8};
+    TreeNode root = TreeNode.makeTree(vals);
+    System.out.println(TreeNode.toString(convertBST(root)));
+  }
+
+  static int temp = 0;
+
+  public static TreeNode convertBST(TreeNode root) {
+    // do an inorder traverse but right first cause the rightest element is biggest
+    // and this element does not need to add anyone to itself
+    if (root == null) {
+      return null;
+    }
+    convertBST(root.right);
+    temp += root.val;
+    root.val = temp;
+    convertBST(root.left);
+    return root;
+  }
+}
